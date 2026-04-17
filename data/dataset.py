5395c03a5c0a8d7c72cb6d4ab82ab54edec593b2
leoschn <schneider.leo@free.fr>
2026-04-17 11:19:52 +0200

    fix dataset load

--- data/dataset.py
+++ data/dataset.py
@@ -565,7 +565,7 @@ class SpectraGraphDatasetPrec(InMemoryDataset):
         self.label_type = label_type
         super().__init__(root, transform, pre_transform)
 
-        self.data, self.slices = torch.load(self.processed_paths[0])
+        self.data, self.slices = torch.load(self.processed_paths[0], weights_only=False)
 
     @property
     def raw_file_names(self):
