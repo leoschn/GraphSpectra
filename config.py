4a0bb4371df65b97436155d6594377fa6570de11
leoschn <schneider.leo@free.fr>
2026-04-20 09:40:28 +0200

    fix streaming_dataset.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ def load_args():
     parser.add_argument('--eval_inter', type=int, default=1)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--batch_size', type=int, default=512)
-    parser.add_argument('--root_train', type=str, default='dataset/processed_graphs_train_hcd_reduced')
-    parser.add_argument('--root_val', type=str, default='dataset/processed_graphs_val_hcd_reduced')
-    parser.add_argument('--root_test', type=str, default='dataset/processed_graphs_holdout_hcd_reduced')
+    parser.add_argument('--root_train', type=str, default='processed_graphs_train_hcd_reduced')
+    parser.add_argument('--root_val', type=str, default='processed_graphs_val_hcd_reduced')
+    parser.add_argument('--root_test', type=str, default='processed_graphs_holdout_hcd_reduced')
     args = parser.parse_args()
 
     return args
