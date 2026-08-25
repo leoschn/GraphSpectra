cba0bec883adb526abe86f76cee69601ef142a7e
leoschn <schneider.leo@free.fr>
2026-08-25 11:21:59 +0200

    fix input precompute ptms

--- precompute_dataset_ptms.py
+++ precompute_dataset_ptms.py
@@ -235,7 +235,7 @@ if __name__ == "__main__":
                   'pr',
                   'ni',
                   'ac',
-                  'gl'
+                  'gl',
                   'su',
                   'ci',
                   'ph',
